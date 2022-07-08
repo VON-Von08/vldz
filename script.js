fca28dab4211d6971d920d2ee835f7378b79d888
VON-Von08 <102305750+VON-Von08@users.noreply.github.com>
2022-07-08 14:53:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* ===== typing animation ==== */
+var typed = new Typed (".typing", {
+    strings:[";","Computer Engineering Student","Pangasinan State University - Urdaneta Campus", ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
